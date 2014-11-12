084cd1f6de85800c923892f8eb860a4ea48a6c37
Robert M. Lefkowitz <r0ml@mac.com>
2014-11-12 12:05:20 -0500

    removed the log service

--- src/net/r0kit/brijj/aBrijj.js
+++ src/net/r0kit/brijj/aBrijj.js
@@ -10,6 +10,7 @@
  */
 
 function xadb(hz, a) { var h = angular.element(hz); var b = Array.prototype.slice.call(arguments,2); var swb = h.injector().get('{{scriptName}}'); h.scope().$apply( function() { swb[a].apply(swb, b).then(function(x) { console.log(x); } ) } ) };
+function id(x) { return x; }
 
 var _bm = angular.module('brijj',[], function($httpProvider) {
     $httpProvider.interceptors.push('errorHttpInterceptor');
@@ -29,7 +30,7 @@ _bm.factory('errorHttpInterceptor',
         return {
             'response': function (response) {
                 if (response.data[0]=='x') {
-                    /** @type { {javaClassName: string, message: string } }*/
+                    /** @type { {javaClassName: string, message: string } } */
                     var errm = eval(response.data.substr(2));
                     if (errm.javaClassName == "net.r0kit.brijj.BrijjServlet$NotLoggedIn") {
                         $rootScope.$broadcast('event:loginRequired');
@@ -52,28 +53,21 @@ _bm.factory('errorHttpInterceptor',
         };
     }]);
 
-_bm.factory('logService', function($rootScope) {
-    return {
-        log: [],
-        sendLog: function(log) {
-            $rootScope.$broadcast('dblog:updated', this.log);
-        },
-        addItem: function(log_item, startTime) {
-            log_item.time = Date.now()-startTime;
-            this.log.push(log_item);
-            this.sendLog();
-            // could restrict this to only send under certain conditions
-        },
-        clearLog: function() {
-            this.log = [];
-        }
-    };
-});
 /* */
 /* bbbb = function(res) { angular.module('aaa',[]).service('bbb', function($q,$http) { ccc.$http = $http; ccc.$q = $q; } ); return ccc; } */
 /* angular.module('main',['aaa']).controller('zzz',function(bbb) {}); */
-_bm.factory('{{scriptName}}', ['$http','$q','$injector', '$rootScope', 'logService', function($http,$q,$injector,$rootScope, logService) {
-    return {
+_bm.factory('{{scriptName}}', ['$http','$q','$injector', '$rootScope', function($http,$q,$injector,$rootScope) {
+
+  
+   var ptfn = function(args) {
+    var url = "/brijj/bridj";
+    var p = JSON.stringify(args);
+    var req = $http.post(url, p);
+    console.log(url+": "+p);
+    return req.then(function(x) { return x.data });
+  }; 
+  
+   return {
         _path: '/brijj/',
     defaultErrorHandler:  // function(x) {alert(x.javaClassName+": "+x.message); },
     function(errm) {
@@ -130,28 +124,10 @@ _bm.factory('{{scriptName}}', ['$http','$q','$injector', '$rootScope', 'logServi
 
         var req = $http.post(url, body);
         req.brijj = this;
-        /*       return {
-                 then: function(x) { req.success( { }); }
-                 }
-                 req.then = req.success;
-                 req.except = req.error;
-                 */     
-        // req.callback = function(x) { console.log(x); };
-        // req.errback = self.defaultErrorHandler;
-
-        /*  if (req.status == 200 || req.status == 0) {
-            switch(req.data[0]) {
-            case 'c': self.handleCallback(callback, eval(toEval.substring(2))); break;
-            case 'x': self.handleException( errorHandler, eval(toEval.substring(2)) ); break;
-            default: alert("unknown server-response type: "+toEval[0]);
-            }
-            }
-            */
 
         return {
-            then: function(x) {
+            then: function(x, errx) {
                 var zfn = function(z) {
-                    logService.addItem(log_item, start);
                     switch( z.data[0]) {
                         case 'c':
                             x.apply(window, [eval(z.data.substring(2))]);
@@ -164,13 +140,20 @@ _bm.factory('{{scriptName}}', ['$http','$q','$injector', '$rootScope', 'logServi
                         default: alert("unknown server-response type: "+z.data[0]);
                     };
                 };
-                req.errorHandler = req.brijj.defaultErrorHandler;
-                req.then( zfn, req.brijj.defaultErrorHandler );
-                return {
+                if (typeof errx == 'undefined') {
+                  req.errorHandler = req.brijj.defaultErrorHandler;
+                  req.then( zfn, req.brijj.defaultErrorHandler );
+                  return {
                     except: function(y) {
                         req.errorHandler = y;
                         req.error(y);
                     } 
+                  }
+                } else {
+                  req.then( zfn, errx);
+                  req.errorHandler = errx;
+                  req.error(errx);
+                  return req;
                 }
             }
         }
@@ -283,77 +266,37 @@ _bm.factory('{{scriptName}}', ['$http','$q','$injector', '$rootScope', 'logServi
         }
     },
 
-    passthrough: function(args) {
-      var url = this._path+"/bridj";
-      var p = JSON.stringify(args);
-      var req = $http.post(url, p);
-      console.log(url+": "+p);
-      return req.then(function(x) { return x.data });
-    }, 
-    
-
+    passthrough: ptfn,
+    proc: function(f) { var p = ptfn({'op':'execute','function':f,'asarray':true,
+      'args':Array.prototype.slice.call(arguments,1) });
+    // the real promise should be:  return p;
+    // however, for the old style: then().except() ....
+      return {
+        then: function(f,fx) {
+          var erf = function(errm) {
+            errm = fixerrm(erm);
+            $rootScope.$broadcast( errm.javaClassName == 'net.r0kit.brijj.BrijjServlet$NotLoggedIn' ?
+                'event:loginRequired' : 'event:brijjError', errm.message);
+            };
+          if (typeof(fx) == 'undefined') {
+            var q = p.then(f,erf);
+            q.except = function(g) {
+              return q.then(id, function(errm) { return g(fixerrm(errm)); } );
+            };
+            return q;
+          } else {
+            return p.then(f, function(errm) { return fx(fixerrm(errm)); } );
+          }
+        }
+      }
+    },
 
     {{functions}}
     }
 }]);
 
-
-/*
-   this.defaultErrorHandler = function(ex,q) {
-   alert((ex.javaClassName ? ex.javaClassName : "" )+": "+ex.message);
-   console.log({error: ex, request: q});
-   }
-
-
-   this.sendIframe = function(url, args, callback, errorHandler) {
-   var div1 = document.createElement("div");
-   document.body.appendChild(div1);
-   div1.innerHTML = "<iframe src='about:blank' frameborder='0' name='xxxxx' style='width:0px;height:0px;border:0;display:none;'></iframe><form encType='multipart/form-data' encoding='multipart/form-data' method='POST' style='display:none'></form>";
-
-   var iframe = div1.firstChild;
-   iframe.callback = function(x) { console.log(x); }
-   iframe.errback = self.defaultErrorHandler;
-   var fr = function() { self.doResponse(iframe.callback, iframe.errback, iframe.contentDocument.body.innerHTML); document.body.removeChild(div1); delete div1; return true; };
-   if (iframe.addEventListener) iframe.addEventListener("load", fr, true);
-   if (iframe.attachEvent) iframe.attachEvent("onload", fr);
-
-   var form = div1.children[1];
-   form.setAttribute("action", url);
-   form.setAttribute("target", "xxxxx");
-   for (var i=0;i<args.length;i++) {
-   var value = args[i];
-   var prop = "p"+i;
-   if (value && value.tagName && value.tagName.toLowerCase() == "input" && value.type && value.type.toLowerCase() == "file") {
-   var clone = value.cloneNode(true);
-   value.removeAttribute("id");
-   value.setAttribute("name", prop);
-   value.style.display = "none";
-   value.parentNode.insertBefore(clone, value);
-   value.parentNode.removeChild(value);
-   form.appendChild(value);
-   } else {
-   var formInput = document.createElement("input");
-   formInput.setAttribute("type", "hidden");
-   formInput.setAttribute("name", prop);
-   formInput.setAttribute("value", value);
-   form.appendChild(formInput);
-   }
-   }
-   form.submit();
-// This generates a potential race condition, the send happens before the callback is set
-return {
-then: function(callback, errback) {
-iframe.callback = callback;
-if (errback != null) iframe.errback = errback;
-return {
-except: function(errback) {
-iframe.errback = errback;
+function fixerrm(errm) {
+  errm.message = errm.message || errm.data;
+  try { errm.javaClassName = (err.data.match(/[^:]*)(:|$)/))[1]; } catch(err) {}
+  return errm;
 }
-}
-}
-}
-};
-};
-
-*/
-
